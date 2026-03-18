9e3359df29fb3f7a77f4ac322b11cc86f535f46c
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2026-03-18 09:21:22 +0800

    Update assignment.sql

--- assignment.sql
+++ assignment.sql
@@ -21,3 +21,40 @@
 
 -- Exercise 8
 
+
+-- Exercise 9
+
+
+-- Exercise 10
+
+
+-- Exercise 11
+
+
+-- Exercise 12
+
+
+-- Exercise 13
+
+
+-- Exercise 14
+
+
+-- Exercise 15
+
+
+-- Exercise 16
+
+
+-- Exercise 17
+
+
+-- Exercise 18
+
+
+-- Exercise 19
+
+
+-- Exercise 20
+
+
